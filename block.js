e11d002bf3aacbc8fad7dc2637f4dd4b2bb872b0
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-09-05 19:18:42 -0400

    implementing SHA256 hashing mechanism

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 	constructor(timestamp, lastHash, hash, data) {
 		this.timestamp = timestamp;
@@ -17,6 +19,18 @@ class Block {
 	//static allows to use function directly without creating instance of a Block class
 	static genesis() {
 		return new this('Genesis time', '-----', '0x777777777777777', []);
+	}
+
+	static mineBlock(lastBlock, data) {
+		const timestamp = Date.now();
+		const lastHash  = lastBlock.hash;
+		const hash = Block.hash(timestamp, lastHash, data);
+
+		return new this(timestamp, lastHash, hash, data);
+	}
+
+	static hash(timestamp, lastHash, data) {
+		return SHA256(`${timestamp}${lastHash}${data}`).toString();
 	}
 }
 
