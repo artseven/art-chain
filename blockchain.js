943a6d76b9f4ba9ac8cb88ab66f565d7327272ef
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-09-07 17:10:49 -0400

    done

--- blockchain.js
+++ blockchain.js
@@ -31,8 +31,13 @@ class Blockchain {
 		if (newChain.length <= this.chain.length) {
 			console.log('Received chain is not longer than the current chain.');
 			return;
+		} else if (!this.isValidChain(newChain)) {
+			console.log('Received chain is not valid');
+			return;
 		}
-		
+		console.log('Replacing blockchain with the new chain');
+		this.chain = newChain;
+
 	}
 }
 
