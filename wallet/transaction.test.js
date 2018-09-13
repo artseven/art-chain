b15e8aacfa2889df62606c0ff8c8686adbd1163e
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-09-13 16:39:33 -0400

    adding test for transactions

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -12,8 +12,6 @@ describe('Transaction', () => {
 	});
 
 	it('outputs the `amount` subtracted from the wallet balance', () => {
-		console.log(transaction);
-		console.log(wallet.publicKey);
 		expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
 		 .toEqual(wallet.balance - amount);
 	});
@@ -21,5 +19,20 @@ describe('Transaction', () => {
 	it('outputs the `amount` added to the recipient', () => {
 		expect(transaction.outputs.find(output => output.address === recipient).amount)
 		 .toEqual(amount);
-	})
+	});
+
+	it('inputs the balance of the wallet', () => {
+		expect(transaction.input.amount).toEqual(wallet.balance);
+	});
+
+	describe('transacting with an amount that exceeds the balance', () => {
+		beforeEach(() => {
+			amount = 50000;
+			transaction = Transaction.newTransaction(wallet, recipient, amount);
+		});
+
+		it('does not create the transaction', () => {
+			expect(transaction).toEqual(undefined);
+		});
+	});
 });
